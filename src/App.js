60dbe87590e0a55d4a8c07f08ddfc83266232526
Mark Erikson <mark@isquaredsoftware.com>
2017-02-24 16:28:06 -0500

    Remove built-in App contents

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                Empty
+            </div>
+
+        );
+    }
 }
 
 export default App;
