6776f546e69c18acaea8b8467ff9b8c2870b47bb
Mark Erikson <mark@isquaredsoftware.com>
2017-02-25 17:23:09 -0500

    Add a reusable CesiumBillboard component

---
+++ src/utils/utils.js
@@ -0,0 +1,23 @@
+export function shallowEqual(objA, objB) {
+    if (objA === objB) {
+        return true;
+    }
+
+    const aKeys = Object.keys(objA);
+    const bKeys = Object.keys(objB);
+    const len = aKeys.length;
+
+    if (bKeys.length !== len) {
+        return false;
+    }
+
+    for (let i = 0; i < len; i++) {
+        const key = aKeys[i];
+
+        if (objA[key] !== objB[key]) {
+            return false;
+        }
+    }
+
+    return true;
+};
